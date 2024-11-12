93f63f53a8a72a654c424bd508ee93d286a996eb
Mark Hazlewood <markhazlewood42@gmail.com>
2024-11-11 21:33:06 -0500

    Created input form and started integrating with TV Maze API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
 const Home = () => {
+    
+    const [searchString, setSearchString] = useState("");
+    
+    const onSearchInputChange = (event) => {
+        setSearchString(event.target.value);
+    }
+
+    const onSearch = async (event) => {
+        // Prevent the default behavior for this event, since this 
+        // is a React app and not a normal web form
+        event.preventDefault();
+
+        // https://api.tvmaze.com/search/shows?q=dogs
+        const requestUrl = `https://api.tvmaze.com/search/shows?q=${searchString}`;
+
+        const response = await fetch(requestUrl);
+        const body = await response.json();
+        console.log(body);
+        
+    }
+
     return <div>
-        Home page
+        <form onSubmit={onSearch}>
+            <input type="text" value={searchString} onChange={onSearchInputChange} />
+            <button type="submit">Search</button>
+        </form>
     </div>
 };
 
